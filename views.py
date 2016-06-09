f0087c343824fa90d12b77fff017f8763fcfeffd
tutor <Tutor@nextu.com>
2016-06-09 13:42:24 -0500

    Intancia flask

--- views.py
+++ views.py
@@ -2,3 +2,12 @@
 # Author: María Clara De La Cuesta
 # Project: Webpage
 
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return
+
+if __name__ == "__main__":
+    app.run()
