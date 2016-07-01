c7fc95d401abf7a0bbcbe849d084f00092b8f9c8
tutor <Tutor@nextu.com>
2016-07-01 14:11:22 -0500

    Añade página de bienvenida de usuario

--- views.py
+++ views.py
@@ -1,13 +1,19 @@
 # File: Views.py
-# Author: María Clara De La Cuesta
+# Author: Maria Clara De La Cuesta
 # Project: Webpage
 
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
+
     return render_template('index.html')
+ 
+@app.route('/<string:name>')
+def welcome(name):
+
+    return render_template('welcome.html', name=name)
     
 if __name__ == "__main__":
     app.run()
